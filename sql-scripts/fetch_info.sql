b3b06246203b6b863603ffa36899f200336f6f20
Thomas Cassidy <gvk9wu@virginia.edu>
2024-10-17 12:33:56 -0400

    brainstormed query ideas

---
+++ sql-scripts/fetch_info.sql
@@ -0,0 +1,19 @@
+-- 1. Query to fetch number of user's playlists
+
+-- 2. Query to fetch number of songs in user playlist
+
+-- 3. Query to see most common artist in user playlists
+
+-- 4. Query to see most common genre for albums saved by user
+
+-- 5. Query to get average duration of song in a user playlist
+
+-- 6. Query to get average popularity of albums saved by user
+
+-- 7. Query to get most common artist for all user saved songs
+
+-- 8. Query to get average follower count of artists in a user playlist
+
+-- 9. Query to get most popular artist, by follower count, in a user playlist
+
+-- 10. Query to get least popular artist, by follower count, in a user playlist
