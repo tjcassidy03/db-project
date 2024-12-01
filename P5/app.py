7ea1c549960516d8bb059096a7b47c81f68942b5
IADouglas <98048207+IADouglas@users.noreply.github.com>
2024-12-01 15:23:42 -0500

    Added route ops blueprint imports for remaining entities.

--- P5/app.py
+++ P5/app.py
@@ -18,5 +18,17 @@ db = SQLAlchemy(app)
 from my_requests.user_ops import user_routes
 app.register_blueprint(user_routes)
 
+from my_requests.artist_ops import artist_ops
+app.register_blueprint(artist_ops)
+
+from my_requests.album_ops import album_ops
+app.register_blueprint(album_ops)
+
+from my_requests.playlist_ops import playlist_ops
+app.register_blueprint(playlist_ops)
+
+from my_requests.song_ops import song_ops
+app.register_blueprint(song_ops)
+
 if __name__ == "__main__":
     app.run(debug=True)
